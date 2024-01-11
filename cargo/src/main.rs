7362848587d2c83eb4f669c1cc7bad10a0850e69
TaduJR <nattysec26@gmail.com>
2024-01-11 15:36:28 +0300

    attributes

---
+++ cargo/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // 12. Cargo
+    // 12.1 Dependencies
+
+    
+    // 12.3 Testing
+
+    // 12.4 Build Scripts
+    /* The build script is simply another Rust file that will be compiled and 
+       invoked prior to compiling anything else in the package. 
+    */
+    // The script provides output via stdout. All lines printed are written to target/debug/build/<pkg>/output.
+}
---
+++ crates/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // 11.1 Creating a Library
+    // rustc --crate-type=lib rary.rs
+
+    // 11.2 Using a Library
+    rary::public_function();    
+}
